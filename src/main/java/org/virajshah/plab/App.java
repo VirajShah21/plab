98da402ad2aa5ad68213f14578d8daf1c2fadec0
Viraj Shah <shahvirajd@gmail.com>
2021-10-19 00:46:57 -0400

    Got starter main code

--- src/main/java/org/virajshah/plab/App.java
+++ src/main/java/org/virajshah/plab/App.java
@@ -1,5 +1,8 @@
 package org.virajshah.plab;
 
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -7,7 +10,18 @@ package org.virajshah.plab;
 public class App {
 
     public static void main(String[] args) {
+        HashMap<Integer, List<Double>> runtimes = new HashMap<>();
 
+        for (int sqSize = 2; sqSize < 1000; sqSize++) {
+            Mat series1 = new Mat(sqSize, sqSize);
+            Mat series2 = new Mat(sqSize, sqSize);
+            MatSIMD parallel1 = new MatSIMD(sqSize, sqSize);
+            MatSIMD parallel2 = new MatSIMD(sqSize, sqSize);
+
+            series1.multiply(series2);
+
+            parallel1.multiply(parallel1);
+        }
     }
 
 }
