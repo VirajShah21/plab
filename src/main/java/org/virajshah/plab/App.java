7294296ce2e632c434800e19538e2b8d2bcdfbe1
Viraj Shah <shahvirajd@gmail.com>
2021-10-19 02:34:34 -0400

    Now gets results for 2 threads - 10 threads

--- src/main/java/org/virajshah/plab/App.java
+++ src/main/java/org/virajshah/plab/App.java
@@ -17,27 +17,35 @@ public class App {
             System.out.printf("Running for matrix with size %s\n", sqSize);
             Mat series1 = new Mat(sqSize, sqSize);
             Mat series2 = new Mat(sqSize, sqSize);
-            FastMult fastMult = new FastMult(series1, series2);
+            long[] trialTimes = new long[10];
 
             long start, stop;
 
             start = System.currentTimeMillis();
             series1.multiply(series2);
             stop = System.currentTimeMillis();
-            runtimes.put(sqSize, new long[] { stop - start, 0 });
+            trialTimes[0] = stop - start;
 
-            start = System.currentTimeMillis();
-            fastMult.multiply();
-            stop = System.currentTimeMillis();
-            runtimes.get(sqSize)[1] = stop - start;
+            for (int numThreads = 2; numThreads < 11; numThreads++) {
+                FastMult fastMult = new FastMult(series1, series2, numThreads);
+
+                start = System.currentTimeMillis();
+                fastMult.multiply();
+                stop = System.currentTimeMillis();
+                trialTimes[numThreads - 1] = stop - start;
+            }
+
+            runtimes.put(sqSize, trialTimes);
         }
 
-        System.out.printf("| %5s | %20s | %20s |\n===========================\n", "Size", "Series (ms)",
-                "Parallel (ms)");
+        System.out.printf("| %5s | %6s | %6s | %6s | %6s | %6s | %6s | %6s | %6s | %6s | %6s |\n", "Size", "Series",
+                "2P", "3P", "4P", "5P", "6P", "7P", "8P", "9P", "10P");
 
         for (Map.Entry<Integer, long[]> entrySet : runtimes.entrySet())
-            System.out.printf("| %5d | %20d | %20d |\n", entrySet.getKey(), entrySet.getValue()[0],
-                    entrySet.getValue()[1]);
+            System.out.printf("| %5d | %6d | %6d | %6d | %6d | %6d | %6d | %6d | %6d | %6d | %6d |\n",
+                    entrySet.getKey(), entrySet.getValue()[0], entrySet.getValue()[1], entrySet.getValue()[2],
+                    entrySet.getValue()[3], entrySet.getValue()[4], entrySet.getValue()[5], entrySet.getValue()[6],
+                    entrySet.getValue()[7], entrySet.getValue()[8], entrySet.getValue()[9]);
 
     }
 
