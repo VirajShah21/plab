f020f7185dfe7aa394be9fb20859bc82c1bb7813
Viraj Shah <shahvirajd@gmail.com>
2021-10-19 03:49:26 -0400

    Downsized results

--- src/main/java/org/virajshah/plab/App.java
+++ src/main/java/org/virajshah/plab/App.java
@@ -13,11 +13,11 @@ public class App {
     public static void main(String[] args) {
         HashMap<Integer, long[]> runtimes = new HashMap<>();
 
-        for (int sqSize = 2; sqSize < 250; sqSize++) {
+        for (int sqSize = 50; sqSize < 200; sqSize++) {
             System.out.printf("Running for matrix with size %s\n", sqSize);
             Mat series1 = new Mat(sqSize, sqSize);
             Mat series2 = new Mat(sqSize, sqSize);
-            long[] trialTimes = new long[10];
+            long[] trialTimes = new long[4];
 
             long start, stop;
 
@@ -26,7 +26,7 @@ public class App {
             stop = System.currentTimeMillis();
             trialTimes[0] = stop - start;
 
-            for (int numThreads = 2; numThreads < 11; numThreads++) {
+            for (int numThreads = 2; numThreads < 5; numThreads++) {
                 FastMult fastMult = new FastMult(series1, series2, numThreads);
 
                 start = System.currentTimeMillis();
@@ -38,14 +38,13 @@ public class App {
             runtimes.put(sqSize, trialTimes);
         }
 
-        System.out.printf("| %5s | %6s | %6s | %6s | %6s | %6s | %6s | %6s | %6s | %6s | %6s |\n", "Size", "Series",
-                "2P", "3P", "4P", "5P", "6P", "7P", "8P", "9P", "10P");
+        System.out.printf("| %5s | %6s | %6s | %6s | %6s |\n", "Size", "Series", "2P", "3P", "4P");
 
         for (Map.Entry<Integer, long[]> entrySet : runtimes.entrySet())
-            System.out.printf("| %5d | %6d | %6d | %6d | %6d | %6d | %6d | %6d | %6d | %6d | %6d |\n",
-                    entrySet.getKey(), entrySet.getValue()[0], entrySet.getValue()[1], entrySet.getValue()[2],
-                    entrySet.getValue()[3], entrySet.getValue()[4], entrySet.getValue()[5], entrySet.getValue()[6],
-                    entrySet.getValue()[7], entrySet.getValue()[8], entrySet.getValue()[9]);
+            System.out.printf("| %5d | %6d | %6d | %6d | %6d |\n", entrySet.getKey(), entrySet.getValue()[0],
+                    entrySet.getValue()[1], entrySet.getValue()[2], entrySet.getValue()[3]);
+
+        Plotter.displayChart(runtimes);
 
     }
 
