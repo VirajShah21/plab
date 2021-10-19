4696aab6e1928b858a03ad800fbb28352d732d2d
Viraj Shah <shahvirajd@gmail.com>
2021-10-19 00:16:48 -0400

    IMport nativelib to app

--- src/main/java/org/virajshah/plab/App.java
+++ src/main/java/org/virajshah/plab/App.java
@@ -5,7 +5,13 @@ package org.virajshah.plab;
  *
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    static {
+        System.loadLibrary("native");
     }
+
+    public static void main(String[] args) {
+        new App().helloWorld();
+    }
+
+    private native void helloWorld();
 }
